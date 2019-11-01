781ac4afa9b2acdfebbf580c81a993839feaf9fc
Test123 <Test123@dyn-160-39-214-174.dyn.columbia.edu>
2019-10-31 21:47:19 -0400

    mysql inserts

---
+++ BoozyBooks/db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO sitewordsDB (firstName, lastName) values ('Jane', 'Austen');
+INSERT INTO sitewordsDB (firstName, lastName) values ('Mark', 'Twain');
+INSERT INTO sitewordsDB (firstName, lastName) values ('Lewis', 'Carroll');
+INSERT INTO sitewordsDB (firstName, lastName) values ('Andre', 'Asselin');
+
+INSERT INTO chapterbooksDB (firstName, lastName) values ('xxx', "yyy");
+INSERT INTO chapterbooksDB (firstName, lastName) values ('xxx', "yyy");
+INSERT INTO chapterbooksDB (firstName, lastName) values ('xxx', "yyy");
+INSERT INTO chapterbooksDB (firstName, lastName) values ('xxx', "yyy");
+INSERT INTO chapterbooksDB (firstName, lastName) values('xxx', "yyy");
+INSERT INTO chapterbooksDB (firstName, lastName) values ('xxx', "yyy");
